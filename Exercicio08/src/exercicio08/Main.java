718678d99fb61e1dfc83c49a11aa43e57c9f51fa
Jeferson Amorim Leite <jamoriml@everis.com>
2020-10-25 21:32:50 -0300

    Alteração no método main

--- Exercicio08/src/exercicio08/Main.java
+++ Exercicio08/src/exercicio08/Main.java
@@ -3,6 +3,22 @@ package exercicio08;
 public class Main {
 
 	public static void main(String[] args) {
+		
+		OrdenaNumeros ordenar = new OrdenaNumeros();
+		
+		ordenar.popularArray();
+		
+		ordenar.ordenar();
+			
+		
+	}
+
+}
+package exercicio08;
+
+public class Main {
+
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
