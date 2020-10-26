382a53d2b7e3e0e235ac817126fef0a9e8591192
Jeferson Amorim Leite <jamoriml@everis.com>
2020-10-25 21:22:16 -0300

    Exercicio08

---
+++ Exercicio08/src/exercicio08/OrdenaNumeros.java
@@ -0,0 +1,27 @@
+package exercicio08;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class OrdenaNumeros {
+
+	public int numeros[] = new int[10];
+	public int i =0;
+
+	
+	public void popularArray() {
+		while(i < 10) {
+			System.out.println("Digite uma sequencias de 10 numero: ");
+			numeros[i] = new Scanner(System.in).nextInt();
+			i++;
+		}
+	
+		
+	}
+	
+	public void ordenar() {
+		for (int numero : numeros) {
+			System.out.println("os numeros ordenados são: "+numero);
+		}
+	}
+	
+}
